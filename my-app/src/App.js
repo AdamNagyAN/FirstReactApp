a28b32a1b70aa9dcbab949bedd498d6d9efbc332
Nagy Ádám <raween2000@gmail.com>
2021-07-06 18:08:51 +0200

    updeta

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,11 @@
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Hello from './components/Hello'
-import Message from './components/Message';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <Message />
-      {/*<Greet name="Bruce" heroName="Batman"><p>Children prop</p></Greet>
-      <Greet name="Adam" heroName="Man"><button>Action</button></Greet>
-      <Welcome name="Bruce" heroName="Batman" />
-      <Welcome name="Adam" heroName="Man" />
-      <Welcome />
-  <Hello />*/}
+      <Counter />
+      
     </div>
   );
 }
